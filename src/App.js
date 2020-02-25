33b7f2429be5ddf9aa4dc78f483ded11a82ae2bb
Gerax Sotelo <gsotelo23@gmail.com>
2020-02-25 15:31:02 -0700

    Change App from fn to class and add state obj

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import SearchBox from './SearchBox';
 import { robots } from './robots'; //files doesn't have default export
 
 
-const App = () => {
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox />
+        <CardList robots={this.state.robots} />
+      </div>
+    )
+  }
 }
 
 export default App;
