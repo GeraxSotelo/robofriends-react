58c807b767c0cca80cc71e088083a81be606be0d
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-01 17:09:09 -0600

    create ErrorBoundry class

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    }
+  }
+
+  //componentDidCatch is kind of like try catch. If anything errors out, it will run this lifecyle hook
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ooops. No good</h1>
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundry;
