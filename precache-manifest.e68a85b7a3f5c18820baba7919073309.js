8016d44308aa2cfc0192425bd3185b1283c65747
Gerax Sotelo <gsotelo23@gmail.com>
2020-04-01 17:48:29 -0600

    Updates

---
+++ precache-manifest.e68a85b7a3f5c18820baba7919073309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5bd16caafa665ff6d9b67daa496f6b",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "25165a93389fb0113ca7",
+    "url": "/robofriends-react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "99896cbf8ae0f86d98c2",
+    "url": "/robofriends-react/static/css/main.0f99df0c.chunk.css"
+  },
+  {
+    "revision": "25165a93389fb0113ca7",
+    "url": "/robofriends-react/static/js/2.7136d570.chunk.js"
+  },
+  {
+    "revision": "99896cbf8ae0f86d98c2",
+    "url": "/robofriends-react/static/js/main.3bcc409e.chunk.js"
+  },
+  {
+    "revision": "660e34e80bf257ff43b7",
+    "url": "/robofriends-react/static/js/runtime-main.dc30ec7a.js"
+  }
+]);
